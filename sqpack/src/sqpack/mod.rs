92c38986d2c61ebd4816bd04a9620ce0ea14cf2e
Inseok Lee <dlunch@gmail.com>
2020-01-01 16:28:06 +0900

    Mount all exs by default

--- sqpack/src/sqpack/mod.rs
+++ sqpack/src/sqpack/mod.rs
@@ -5,63 +5,49 @@ mod parser;
 mod reference;
 
 use std::collections::HashMap;
-use std::fs::File;
+use std::ffi::OsStr;
 use std::io;
 use std::path::Path;
 
 use crate::package::Package;
 
-use self::archive::SqPackArchiveId;
-use self::index::SqPackIndex;
+use self::archive::{SqPackArchive, SqPackArchiveId};
 use self::reference::SqPackFileReference;
-
-struct SqPackArchive {
-    pub index: SqPackIndex,
-    pub data: Vec<File>,
-}
 
 pub struct SqPack {
     archives: HashMap<SqPackArchiveId, SqPackArchive>,
 }
 
 impl SqPack {
-    pub fn new() -> SqPack {
-        SqPack {
-            archives: HashMap::new(),
-        }
-    }
+    pub fn new(base_dir: &Path) -> io::Result<SqPack> {
+        let root_dirs = base_dir
+            .read_dir()?
+            .filter_map(Result::ok)
+            .map(|x| x.path())
+            .filter(|x| {
+                let file_name = x.file_name().and_then(OsStr::to_str).unwrap();
+                file_name == "ffxiv" || file_name.starts_with("ex")
+            });
 
-    pub fn mount(&mut self, path: &Path) -> io::Result<()> {
-        let path_str = path.to_str().unwrap();
-        let index_path = format!("{}.index", path_str);
-        let index = SqPackIndex::new(Path::new(&index_path))?;
+        let entries = root_dirs.flat_map(|x| {
+            x.read_dir()
+                .unwrap()
+                .filter_map(Result::ok)
+                .map(|y| y.path())
+                .filter(|y| y.extension().and_then(OsStr::to_str).unwrap() == "index")
+        });
 
-        let data = (0..index.dat_count)
-            .map(|x| File::open(format!("{}.dat{}", path_str, x)))
-            .collect::<Result<Vec<_>, _>>()?;
-        self.archives
-            .insert(SqPack::get_archive_id(path), SqPackArchive { index, data });
+        let archives = entries
+            .map(|x| Ok((SqPack::get_archive_id(&x), SqPackArchive::new(&x)?)))
+            .collect::<io::Result<HashMap<_, _>>>()?;
 
-        Ok(())
-    }
-
-    fn do_read_file(&self, reference: &SqPackFileReference) -> io::Result<Vec<u8>> {
-        let archive = self
-            .archives
-            .get(&reference.archive_id)
-            .ok_or_else(|| io::Error::new(io::ErrorKind::NotFound, "No such archive"))?;
-
-        let file_offset = archive.index.find_offset(reference)?;
-
-        let dat_index = (file_offset & 0x0f) >> 1;
-        let offset = (file_offset & 0xffff_fff0) << 3;
-
-        Ok(Vec::new())
+        Ok(SqPack { archives })
     }
 
     fn get_archive_id(path: &Path) -> SqPackArchiveId {
-        let file_name = path.file_stem().unwrap().to_str().unwrap();
-        let archive_id = u32::from_str_radix(file_name, 16).unwrap();
+        let file_name = path.file_stem().and_then(OsStr::to_str).unwrap();
+        let archive_id_str = file_name.split('.').next().unwrap();
+        let archive_id = u32::from_str_radix(archive_id_str, 16).unwrap();
 
         // ex) 0a0000
         let root = (archive_id / 0x10000) as u8;
@@ -74,7 +60,13 @@ impl SqPack {
 
 impl Package for SqPack {
     fn read_file(&self, path: &str) -> io::Result<Vec<u8>> {
-        self.do_read_file(&SqPackFileReference::new(path))
+        let reference = SqPackFileReference::new(path);
+        let archive = self
+            .archives
+            .get(&reference.archive_id)
+            .ok_or_else(|| io::Error::new(io::ErrorKind::NotFound, "No such archive"))?;
+
+        archive.read_file(&SqPackFileReference::new(path))
     }
 }
 
@@ -85,13 +77,8 @@ mod tests {
     use std::path::Path;
     #[test]
     fn test_read() {
-        let mut pack = SqPack::new();
-        pack.mount(Path::new(
-            "D:\\Games\\FINAL FANTASY XIV - KOREA\\game\\sqpack\\ffxiv\\0a0000.win32",
-        ))
-        .unwrap();
-        pack.mount(Path::new(
-            "D:\\Games\\FINAL FANTASY XIV - KOREA\\game\\sqpack\\ex1\\020101.win32",
+        let pack = SqPack::new(Path::new(
+            "D:\\Games\\FINAL FANTASY XIV - KOREA\\game\\sqpack",
         ))
         .unwrap();
 
