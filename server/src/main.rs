8157408047abf5497b29ed3532b0abf841c66fbf
Inseok Lee <dlunch@gmail.com>
2020-02-22 19:56:56 +0900

    Add server

---
+++ server/src/main.rs
@@ -0,0 +1,7 @@
+use warp::Filter;
+#[tokio::main]
+async fn main() {
+    let hello = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
+
+    warp::serve(hello).run(([127, 0, 0, 1], 8080)).await;
+}
