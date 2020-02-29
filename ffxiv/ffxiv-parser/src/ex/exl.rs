84ce39833e7005a00998501bceecf0783570a66e
Inseok Lee <dlunch@gmail.com>
2020-02-29 17:01:28 +0900

    Add exl parser

---
+++ ffxiv/ffxiv-parser/src/ex/exl.rs
@@ -0,0 +1,19 @@
+use std::io;
+use std::io::{BufRead, Cursor};
+
+use sqpack::Package;
+
+pub struct ExList {
+    pub ex_names: Vec<String>,
+}
+
+impl ExList {
+    pub async fn new(package: &dyn Package) -> io::Result<Self> {
+        let data = package.read_file("exd/root.exl").await?;
+
+        let cursor = Cursor::new(data);
+        let ex_names = cursor.lines().skip(1).map(|x| Ok(x?.split(',').nth(0).unwrap().to_owned())).collect::<io::Result<Vec<_>>>()?;
+
+        Ok(Self {ex_names})
+    }
+}
--- ffxiv/ffxiv-parser/src/lib.rs
+++ ffxiv/ffxiv-parser/src/lib.rs
@@ -1,1 +1,3 @@
+mod ex;
 
+pub use ex::*;
