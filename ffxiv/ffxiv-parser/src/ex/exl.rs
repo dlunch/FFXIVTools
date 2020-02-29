df56b701ceac4b9945a82e9b34b31cf598e2ae7e
Inseok Lee <dlunch@gmail.com>
2020-02-29 17:55:14 +0900

    fmt

--- ffxiv/ffxiv-parser/src/ex/exl.rs
+++ ffxiv/ffxiv-parser/src/ex/exl.rs
@@ -12,8 +12,12 @@ impl ExList {
         let data = package.read_file("exd/root.exl").await?;
 
         let cursor = Cursor::new(data);
-        let ex_names = cursor.lines().skip(1).map(|x| Ok(x?.split(',').nth(0).unwrap().to_owned())).collect::<io::Result<Vec<_>>>()?;
+        let ex_names = cursor
+            .lines()
+            .skip(1)
+            .map(|x| Ok(x?.split(',').nth(0).unwrap().to_owned()))
+            .collect::<io::Result<Vec<_>>>()?;
 
-        Ok(Self {ex_names})
+        Ok(Self { ex_names })
     }
 }
