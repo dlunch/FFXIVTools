73b98bf8c0cb21d69e596ebd67f0387eec6131d0
Inseok Lee <dlunch@gmail.com>
2020-03-04 18:45:21 +0900

    Clippy

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,11 +1,11 @@
 use std::io;
 
-use dotenv;
+use dotenv::dotenv;
 use warp::Filter;
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    dotenv::dotenv().ok();
+    dotenv().ok();
 
     let hello = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
 
