bae2e35e47e30be623becbbdd63988e0d620e75d
Inseok Lee <dlunch@gmail.com>
2020-03-05 21:46:18 +0900

    Use actix

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,21 +1,36 @@
-use std::convert::Infallible;
 use std::io;
 
+use actix_web::{get, web, App, HttpResponse, HttpServer, Result};
 use dotenv::dotenv;
-use warp::Filter;
 
-async fn get_exl() -> Result<impl warp::Reply, Infallible> {
-    Ok("")
+struct Context {
 }
 
-#[tokio::main]
+impl Context {
+    async fn new() -> Self {
+        Self { }
+    }
+}
+
+#[get("/parsed/exl")]
+async fn get_exl() -> Result<HttpResponse> {
+    Ok(HttpResponse::Ok().body("exl"))
+}
+
+#[get("/parsed/ex")]
+async fn get_ex() -> Result<HttpResponse> {
+    Ok(HttpResponse::Ok().body("ex"))
+}
+
+#[actix_rt::main]
 async fn main() -> io::Result<()> {
     dotenv().ok();
 
-    let exl = warp::get().and(warp::path!("parsed" / "exl")).and_then(get_exl);
-    let routes = exl;
+    let context = Context::new().await;
+    let data = web::Data::new(context);
 
-    warp::serve(routes).run(([127, 0, 0, 1], 8080)).await;
-
-    Ok(())
+    HttpServer::new(move || App::new().app_data(data.clone()).service(get_exl).service(get_ex))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
