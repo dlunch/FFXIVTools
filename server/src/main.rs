203ba9a49be42273a33d2783b0f662fcc92d9b32
Inseok Lee <dlunch@gmail.com>
2020-03-05 16:18:51 +0900

    Add exl route

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,15 +1,21 @@
+use std::convert::Infallible;
 use std::io;
 
 use dotenv::dotenv;
 use warp::Filter;
 
+async fn get_exl() -> Result<impl warp::Reply, Infallible> {
+    Ok("")
+}
+
 #[tokio::main]
 async fn main() -> io::Result<()> {
     dotenv().ok();
 
-    let hello = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
+    let exl = warp::get().and(warp::path!("parsed" / "exl")).and_then(get_exl);
+    let routes = exl;
 
-    warp::serve(hello).run(([127, 0, 0, 1], 8080)).await;
+    warp::serve(routes).run(([127, 0, 0, 1], 8080)).await;
 
     Ok(())
 }
