e7c8b49a207ed331020edaf7e79d6d6b71fac9e3
Inseok Lee <dlunch@gmail.com>
2020-03-08 19:39:48 +0900

    Return io::Result on test

--- ffxiv/ffxiv_parser/tests/exl_test.rs
+++ ffxiv/ffxiv_parser/tests/exl_test.rs
@@ -1,20 +1,23 @@
 #[cfg(test)]
 mod tests {
     use std::collections::HashSet;
+    use std::io;
     use std::iter::FromIterator;
 
     use sqpack_reader::{FileProviderWeb, SqPackReaderFile};
 
     use ffxiv_parser::ExList;
     #[tokio::test]
-    async fn test_exl() {
+    async fn test_exl() -> io::Result<()> {
         let provider = FileProviderWeb::new("https://ffxiv-data3.dlunch.net/compressed/");
-        let pack = SqPackReaderFile::new(provider).unwrap();
+        let pack = SqPackReaderFile::new(provider)?;
 
-        let ex_list = ExList::new(&pack).await.unwrap();
+        let ex_list = ExList::new(&pack).await?;
         let ex_set: HashSet<String> = HashSet::from_iter(ex_list.ex_names);
 
         assert!(ex_set.contains("Item"));
         assert!(ex_set.contains("opening/OpeningGridania"));
+
+        Ok(())
     }
 }
