a4791fd8d6aa40bfd7fa2e3ac909851427f05eb4
Inseok Lee <dlunch@gmail.com>
2020-03-08 14:47:31 +0900

    read column header

--- ffxiv/ffxiv_parser/src/ex_parser/ex.rs
+++ ffxiv/ffxiv_parser/src/ex_parser/ex.rs
@@ -3,18 +3,20 @@ use std::io;
 use bytes::Buf;
 use sqpack_reader::Package;
 
-use super::definition::ExhHeader;
+use super::definition::{ExhColumnHeader, ExhHeader};
 
+#[allow(dead_code)] // WIP
 pub struct Ex {
-    #[allow(dead_code)] // WIP
     header: ExhHeader,
+    columns: Vec<ExhColumnHeader>,
 }
 
 impl Ex {
     pub async fn new(package: &dyn Package, name: &str) -> io::Result<Self> {
         let mut data = package.read_file(&format!("exd/{}.exh", name)).await?;
-        let header = parse!(data, 0, ExhHeader);
+        let header = parse!(data, ExhHeader);
+        let columns = parse!(data, header.column_count as usize, ExhColumnHeader);
 
-        Ok(Self { header })
+        Ok(Self { header, columns })
     }
 }
