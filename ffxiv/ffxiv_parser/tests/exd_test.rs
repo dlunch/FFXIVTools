b0442918149a31cc069ceffa777e35e0f7ca4877
Inseok Lee <dlunch@gmail.com>
2020-03-09 18:55:07 +0900

    Use cached uri for test

--- ffxiv/ffxiv_parser/tests/exd_test.rs
+++ ffxiv/ffxiv_parser/tests/exd_test.rs
@@ -6,7 +6,7 @@ mod tests {
     use ffxiv_parser::Ex;
     #[tokio::test]
     async fn test_exd() -> io::Result<()> {
-        let provider = FileProviderWeb::new("https://ffxiv-data3.dlunch.net/compressed/");
+        let provider = FileProviderWeb::new("https://ffxiv-data.dlunch.net/compressed/");
         let pack = SqPackReaderFile::new(provider)?;
 
         let _ex = Ex::new(&pack, "classjob").await?;
