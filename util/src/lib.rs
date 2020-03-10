c2beb58b9840150358cd20a1e27af205942d7c28
Inseok Lee <dlunch@gmail.com>
2020-03-10 18:00:47 +0900

    fmt

--- util/src/lib.rs
+++ util/src/lib.rs
@@ -27,7 +27,9 @@ macro_rules! parse {
         <$type>::parse(&$data).unwrap().1
     };
     ($data: expr, $count: expr, $type: ty) => {
-        (0..$count as usize).map(|x| $crate::parse!(&$data[x * <$type>::SIZE..], $type)).collect::<Vec<_>>()
+        (0..$count as usize)
+            .map(|x| $crate::parse!(&$data[x * <$type>::SIZE..], $type))
+            .collect::<Vec<_>>()
     };
 }
 
