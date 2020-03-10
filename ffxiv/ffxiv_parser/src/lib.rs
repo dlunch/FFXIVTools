12493f6074ab7b56fbac7589b543dd4910c74f97
Inseok Lee <dlunch@gmail.com>
2020-03-10 15:39:46 +0900

    Do not use num

--- ffxiv/ffxiv_parser/src/lib.rs
+++ ffxiv/ffxiv_parser/src/lib.rs
@@ -4,9 +4,8 @@ mod util;
 mod ex;
 
 use enum_map::Enum;
-use num_derive::FromPrimitive;
 
-#[derive(FromPrimitive, Enum, Clone, Copy, PartialEq, Eq, Hash)]
+#[derive(Enum, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum Language {
     None = 0,
     Japanese = 1,
@@ -16,6 +15,22 @@ pub enum Language {
     ChineseSimplified = 5,
     ChineseTraditional = 6,
     Korean = 7,
+}
+
+impl Language {
+    pub fn from_raw(raw: u16) -> Self {
+        match raw {
+            0 => Language::None,
+            1 => Language::Japanese,
+            2 => Language::English,
+            3 => Language::Deutsch,
+            4 => Language::French,
+            5 => Language::ChineseSimplified,
+            6 => Language::ChineseTraditional,
+            7 => Language::Korean,
+            _ => panic!(),
+        }
+    }
 }
 
 pub use ex::{Ex, ExList};
