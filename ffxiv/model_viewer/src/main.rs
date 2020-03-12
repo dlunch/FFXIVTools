aa85d972ba58bab3bf7337b5b8beb0c5f7312a80
Inseok Lee <dlunch@gmail.com>
2020-03-12 19:32:13 +0900

    Copy and paste some wgpu basics

--- ffxiv/model_viewer/src/main.rs
+++ ffxiv/model_viewer/src/main.rs
@@ -1,5 +1,48 @@
+use renderer::Renderer;
+
 #[cfg(feature = "binary")]
-fn main() {}
+fn main() {
+    use winit::{
+        event,
+        event::WindowEvent,
+        event_loop::{ControlFlow, EventLoop},
+    };
+
+    let event_loop = EventLoop::new();
+
+    let mut builder = winit::window::WindowBuilder::new();
+    builder = builder.with_title("test");
+    let window = builder.build(&event_loop).unwrap();
+
+    let renderer = Renderer::new(&window);
+
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Exit;
+
+        match event {
+            event::Event::MainEventsCleared => window.request_redraw(),
+            event::Event::WindowEvent { event, .. } => match event {
+                WindowEvent::KeyboardInput {
+                    input:
+                        event::KeyboardInput {
+                            virtual_keycode: Some(event::VirtualKeyCode::Escape),
+                            state: event::ElementState::Pressed,
+                            ..
+                        },
+                    ..
+                }
+                | WindowEvent::CloseRequested => {
+                    *control_flow = ControlFlow::Exit;
+                }
+                _ => {}
+            },
+            event::Event::RedrawRequested(_) => {
+                renderer.redraw();
+            }
+            _ => {}
+        }
+    });
+}
 
 #[cfg(not(feature = "binary"))]
 fn main() {}
