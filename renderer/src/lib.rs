e588d7f1a4e25c6ed8f2fb026c37a5863853532b
Inseok Lee <dlunch@gmail.com>
2020-03-12 19:32:13 +0900

    Copy and paste some wgpu basics

--- renderer/src/lib.rs
+++ renderer/src/lib.rs
@@ -1,1 +1,40 @@
+use raw_window_handle::HasRawWindowHandle;
 
+#[allow(dead_code)]
+pub struct Renderer {
+    device: wgpu::Device,
+    swap_chain: wgpu::SwapChain,
+    queue: wgpu::Queue,
+}
+
+impl Renderer {
+    pub fn new<W: HasRawWindowHandle>(window: &W) -> Self {
+        let surface = wgpu::Surface::create(window);
+
+        let adapter = wgpu::Adapter::request(&wgpu::RequestAdapterOptions {
+            power_preference: wgpu::PowerPreference::Default,
+            backends: wgpu::BackendBit::PRIMARY,
+        })
+        .unwrap();
+
+        let (device, queue) = adapter.request_device(&wgpu::DeviceDescriptor {
+            extensions: wgpu::Extensions {
+                anisotropic_filtering: false,
+            },
+            limits: wgpu::Limits::default(),
+        });
+
+        let sc_desc = wgpu::SwapChainDescriptor {
+            usage: wgpu::TextureUsage::OUTPUT_ATTACHMENT,
+            format: wgpu::TextureFormat::Bgra8UnormSrgb,
+            width: 1024,
+            height: 768,
+            present_mode: wgpu::PresentMode::Vsync,
+        };
+        let swap_chain = device.create_swap_chain(&surface, &sc_desc);
+
+        Self { device, swap_chain, queue }
+    }
+
+    pub fn redraw(&self) {}
+}
