e52e6f696a163ecdbf1da1af81ec8d903b70c6a2
Inseok Lee <dlunch@gmail.com>
2020-03-14 15:51:07 +0900

    Pour cube example from wgpu

--- ffxiv/model_viewer/src/main.rs
+++ ffxiv/model_viewer/src/main.rs
@@ -14,29 +14,33 @@ fn main() {
     builder = builder.with_title("test");
     let window = builder.build(&event_loop).unwrap();
 
-    let renderer = Renderer::new(&window);
+    let mut renderer = Renderer::new(&window);
 
-    event_loop.run(move |event, _, control_flow| match event {
-        event::Event::MainEventsCleared => window.request_redraw(),
-        event::Event::WindowEvent { event, .. } => match event {
-            WindowEvent::KeyboardInput {
-                input:
-                    event::KeyboardInput {
-                        virtual_keycode: Some(event::VirtualKeyCode::Escape),
-                        state: event::ElementState::Pressed,
-                        ..
-                    },
-                ..
-            }
-            | WindowEvent::CloseRequested => {
-                *control_flow = ControlFlow::Exit;
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Poll;
+
+        match event {
+            event::Event::MainEventsCleared => window.request_redraw(),
+            event::Event::WindowEvent { event, .. } => match event {
+                WindowEvent::KeyboardInput {
+                    input:
+                        event::KeyboardInput {
+                            virtual_keycode: Some(event::VirtualKeyCode::Escape),
+                            state: event::ElementState::Pressed,
+                            ..
+                        },
+                    ..
+                }
+                | WindowEvent::CloseRequested => {
+                    *control_flow = ControlFlow::Exit;
+                }
+                _ => {}
+            },
+            event::Event::RedrawRequested(_) => {
+                renderer.redraw();
             }
             _ => {}
-        },
-        event::Event::RedrawRequested(_) => {
-            renderer.redraw();
         }
-        _ => {}
     });
 }
 
