12837eaefd70a63c7fb9d2d46de810af73abd16b
Inseok Lee <dlunch@gmail.com>
2020-03-17 16:58:31 +0900

    Split server into files

---
+++ server/src/context.rs
@@ -0,0 +1,28 @@
+use std::error::Error;
+use std::path::Path;
+
+use actix_web::Result;
+use dotenv::dotenv;
+
+use sqpack_reader::Package;
+
+pub struct Context {
+    pub package: Box<dyn Package>,
+}
+
+impl Context {
+    pub async fn new() -> Result<Self, Box<dyn Error>> {
+        dotenv().ok();
+
+        #[cfg(unix)]
+        let package = Box::new(sqpack_reader::SqPackReaderFile::new(sqpack_reader::FileProviderFile::new(Path::new(
+            "/mnt/i/FFXIVData/data/kor_505",
+        )))?);
+        #[cfg(windows)]
+        let package = Box::new(sqpack_reader::SqPackReader::new(Path::new(
+            "D:\\Games\\FINAL FANTASY XIV - KOREA\\game\\sqpack",
+        ))?);
+
+        Ok(Self { package })
+    }
+}
