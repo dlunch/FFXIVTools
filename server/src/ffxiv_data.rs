39576501e489c04898b88ac6a508f3aece559045
Inseok Lee <dlunch@gmail.com>
2020-03-17 20:54:48 +0900

    Return as json list

--- server/src/ffxiv_data.rs
+++ server/src/ffxiv_data.rs
@@ -1,14 +1,13 @@
-use actix_web::{web, HttpResponse, Result};
+use actix_web::{web, HttpResponse, Responder, Result};
 
 use ffxiv_parser::ExList;
 
 use super::context::Context;
 
-async fn get_exl(context: web::Data<Context>) -> Result<HttpResponse> {
+async fn get_exl(context: web::Data<Context>) -> Result<impl Responder> {
     let exl = ExList::new(&*context.package).await?;
-    let exl_str = exl.ex_names.join("\n");
 
-    Ok(HttpResponse::Ok().body(exl_str))
+    Ok(web::Json(exl.ex_names))
 }
 
 async fn get_ex() -> Result<HttpResponse> {
