73e4464ca10c89d62fee8eee4fef2637040d62e5
Inseok Lee <dlunch@gmail.com>
2020-03-18 21:02:24 +0900

    Return iterator

--- ffxiv/ffxiv_parser/src/ex/exd.rs
+++ ffxiv/ffxiv_parser/src/ex/exd.rs
@@ -36,11 +36,8 @@ impl ExData {
         Some(data.data)
     }
 
-    pub fn read_all(&self) -> BTreeMap<u32, &[u8]> {
-        self.offsets
-            .iter()
-            .map(|x| (*x.0, self.read_row(*x.0).unwrap()))
-            .collect::<BTreeMap<_, _>>()
+    pub fn read_all(&self) -> impl Iterator<Item = (u32, &[u8])> {
+        self.offsets.iter().map(move |x| (*x.0, self.read_row(*x.0).unwrap()))
     }
 
     fn language_to_suffix(language: Language) -> &'static str {
