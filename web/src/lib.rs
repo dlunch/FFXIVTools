6d415bb88adbb0daca26c0a0670c70344ab8d2ff
Inseok Lee <dlunch@gmail.com>
2020-03-20 17:29:14 +0900

    Add yew

--- web/src/lib.rs
+++ web/src/lib.rs
@@ -1,15 +1,14 @@
-use wasm_bindgen::prelude::*;
-use web_sys::console;
+mod app;
+
+use wasm_bindgen::prelude::wasm_bindgen;
 
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen(start)]
-pub fn main() -> Result<(), JsValue> {
+pub fn main() {
     #[cfg(debug_assertions)]
     console_error_panic_hook::set_once();
 
-    console::log_1(&JsValue::from_str("Hello world!"));
-
-    Ok(())
+    yew::start_app::<app::App>();
 }
