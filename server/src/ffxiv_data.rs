af43ac6f2ebe21f23bf7b5f0261bcec48bb71195
Inseok Lee <dlunch@gmail.com>
2020-03-29 07:51:28 +0900

    Specify language at path parameter

--- server/src/ffxiv_data.rs
+++ server/src/ffxiv_data.rs
@@ -2,13 +2,20 @@ mod context;
 
 use std::collections::BTreeMap;
 
-use actix_web::{http::StatusCode, web, HttpResponse, Responder, Result};
+use actix_web::{error, http::StatusCode, web, HttpResponse, Responder, Result};
 use lazy_static::lazy_static;
+use serde::Deserialize;
 use serde_json;
 
-use ffxiv_parser::{Ex, ExList};
+use ffxiv_parser::{Ex, ExList, Language};
 
 use context::Context;
+
+#[derive(Deserialize)]
+struct GetExParameter {
+    ex_name: String,
+    language: Option<Language>,
+}
 
 lazy_static! {
     static ref CONTEXT: Context = Context::new().unwrap();
@@ -20,12 +27,19 @@ async fn get_exl(context: Context) -> Result<impl Responder> {
     Ok(web::Json(exl.ex_names))
 }
 
-async fn get_ex(context: Context, param: web::Path<(String,)>) -> Result<impl Responder> {
-    let ex_name = &param.0;
-    let ex = Ex::new(&context.package, ex_name).await?;
+async fn get_ex(context: Context, param: web::Path<GetExParameter>) -> Result<impl Responder> {
+    let ex = Ex::new(&context.package, &param.ex_name).await?;
 
-    let ex_data = ex.languages().iter().map(|&x| (x as u32, ex.all(x).unwrap())).collect::<BTreeMap<_, _>>();
+    let languages = if let Some(language) = param.language {
+        if !ex.languages().iter().any(|&x| x == language) {
+            return Err(error::ErrorNotFound("No such language"));
+        }
+        vec![language]
+    } else {
+        Vec::from(ex.languages())
+    };
 
+    let ex_data = languages.iter().map(|&x| (x as u32, ex.all(x).unwrap())).collect::<BTreeMap<_, _>>();
     let ex_json = serde_json::to_string(&ex_data)?;
 
     // not using `web::Json` because `web::Json` takes ownership and return value of `ex.read_all()` requires same lifetime as `ex`.
@@ -35,5 +49,6 @@ async fn get_ex(context: Context, param: web::Path<(String,)>) -> Result<impl Re
 pub fn config(cfg: &mut web::ServiceConfig) {
     cfg.data(CONTEXT.clone())
         .service(web::resource("/parsed/exl").route(web::get().to(get_exl)))
-        .service(web::resource("/parsed/ex/{ex_name}").route(web::get().to(get_ex)));
+        .service(web::resource("/parsed/ex/{ex_name}").route(web::get().to(get_ex)))
+        .service(web::resource("/parsed/ex/{language}/{ex_name}").route(web::get().to(get_ex)));
 }
