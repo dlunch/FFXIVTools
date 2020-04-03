165e259815f9d5ac4cc6d465ffbf7ff804d707ce
Inseok Lee <dlunch@gmail.com>
2020-04-03 20:40:47 +0900

    Use archive

--- libs/sqpack_reader/src/sqpack_reader.rs
+++ libs/sqpack_reader/src/sqpack_reader.rs
@@ -6,6 +6,7 @@ mod index;
 
 use std::io;
 use std::path::Path;
+use std::sync::Arc;
 
 use async_trait::async_trait;
 use bytes::Bytes;
@@ -14,6 +15,7 @@ use crate::archive_id::SqPackArchiveId;
 use crate::package::Package;
 use crate::reference::SqPackFileReference;
 
+use self::archive::SqPackArchive;
 use self::archive_container::SqPackArchiveContainer;
 
 pub struct SqPackReader {
@@ -27,23 +29,8 @@ impl SqPackReader {
         })
     }
 
-    pub async fn folders(&self, archive_id: SqPackArchiveId) -> io::Result<Vec<u32>> {
-        let archive = self.archives.get_archive(archive_id).await?;
-
-        Ok(archive.folders().collect::<Vec<_>>())
-    }
-
-    pub async fn files(&self, archive_id: SqPackArchiveId, folder_hash: u32) -> io::Result<Vec<u32>> {
-        let archive = self.archives.get_archive(archive_id).await?;
-        let files = archive.files(folder_hash)?;
-
-        Ok(files.collect::<Vec<_>>())
-    }
-
-    pub async fn read_as_compressed_by_archive(&self, archive_id: SqPackArchiveId, folder_hash: u32, file_hash: u32) -> io::Result<Bytes> {
-        let archive = self.archives.get_archive(archive_id).await?;
-
-        archive.read_as_compressed(folder_hash, file_hash).await
+    pub async fn archive(&self, archive_id: SqPackArchiveId) -> io::Result<Arc<SqPackArchive>> {
+        self.archives.get_archive(archive_id).await
     }
 }
 
@@ -56,7 +43,8 @@ impl Package for SqPackReader {
     }
 
     async fn read_as_compressed_by_reference(&self, reference: &SqPackFileReference) -> io::Result<Bytes> {
-        self.read_as_compressed_by_archive(reference.archive_id, reference.hash.folder, reference.hash.file)
-            .await
+        let archive = self.archives.get_archive(reference.archive_id).await?;
+
+        archive.read_as_compressed(reference.hash.folder, reference.hash.file).await
     }
 }
--- tools/sqpack_extractor/src/main.rs
+++ tools/sqpack_extractor/src/main.rs
@@ -18,16 +18,16 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let package = SqPackReader::new(Path::new(matches.value_of("base_path").unwrap()))?;
 
     let archive_id = SqPackArchiveId::from_file_path(matches.value_of("root").unwrap());
-    let folders = package.folders(archive_id).await?;
+    let archive = package.archive(archive_id).await?;
 
-    future::join_all(folders.into_iter().map(|folder_hash| {
-        let package = &package;
+    future::join_all(archive.folders().map(|folder_hash| {
+        let archive = &archive;
         async move {
             fs::create_dir(folder_hash.to_string()).await?;
-            let files = package.files(archive_id, folder_hash).await?;
+            let files = archive.files(folder_hash)?;
 
-            future::join_all(files.into_iter().map(|file_hash| async move {
-                let data = package.read_as_compressed_by_archive(archive_id, folder_hash, file_hash).await?;
+            future::join_all(files.map(|file_hash| async move {
+                let data = archive.read_as_compressed(folder_hash, file_hash).await?;
                 let path = format!("{}/{}", folder_hash, file_hash);
 
                 println!("{}", path);
