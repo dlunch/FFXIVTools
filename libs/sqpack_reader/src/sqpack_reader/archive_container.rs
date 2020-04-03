d18941e641d5c94ec29af23c6200e5f637f7767a
Inseok Lee <dlunch@gmail.com>
2020-04-03 20:55:36 +0900

    Use RwLock

--- libs/sqpack_reader/src/sqpack_reader/archive_container.rs
+++ libs/sqpack_reader/src/sqpack_reader/archive_container.rs
@@ -5,14 +5,14 @@ use std::path::{Path, PathBuf};
 use std::sync::Arc;
 
 use log::debug;
-use tokio::sync::Mutex;
+use tokio::sync::RwLock;
 
 use super::archive::SqPackArchive;
 use crate::archive_id::SqPackArchiveId;
 
 pub struct SqPackArchiveContainer {
     archive_paths: HashMap<SqPackArchiveId, PathBuf>,
-    archives: Mutex<HashMap<SqPackArchiveId, Arc<SqPackArchive>>>,
+    archives: RwLock<HashMap<SqPackArchiveId, Arc<SqPackArchive>>>,
 }
 
 impl SqPackArchiveContainer {
@@ -36,20 +36,22 @@ impl SqPackArchiveContainer {
 
         Ok(Self {
             archive_paths,
-            archives: Mutex::new(HashMap::new()),
+            archives: RwLock::new(HashMap::new()),
         })
     }
 
     pub async fn get_archive(&self, archive_id: SqPackArchiveId) -> io::Result<Arc<SqPackArchive>> {
-        let mut archives = self.archives.lock().await;
-
-        if !archives.contains_key(&archive_id) {
-            let archive = SqPackArchive::new(self.archive_paths.get(&archive_id).unwrap()).await?;
-            archives.insert(archive_id, Arc::new(archive));
+        {
+            let archives = self.archives.read().await;
+            if archives.contains_key(&archive_id) {
+                return Ok(archives.get(&archive_id).unwrap().clone());
+            }
         }
 
-        let archive = archives.get(&archive_id).unwrap();
+        let mut archives = self.archives.write().await;
+        let archive = SqPackArchive::new(self.archive_paths.get(&archive_id).unwrap()).await?;
+        archives.insert(archive_id, Arc::new(archive));
 
-        Ok(archive.clone())
+        Ok(archives.get(&archive_id).unwrap().clone())
     }
 }
