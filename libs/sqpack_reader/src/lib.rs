d9f5d0dca9b94b2100a018216b0482e78ca76f4a
Inseok Lee <dlunch@gmail.com>
2020-04-04 16:42:29 +0900

    use cfg_if

--- libs/sqpack_reader/src/lib.rs
+++ libs/sqpack_reader/src/lib.rs
@@ -3,16 +3,18 @@ mod extracted_file_provider;
 mod package;
 mod raw_file;
 mod reference;
-#[cfg(feature = "std")]
-mod sqpack_reader;
 mod sqpack_reader_extracted_file;
 
 pub use self::archive_id::SqPackArchiveId;
 pub use self::extracted_file_provider::ExtractedFileProvider;
-#[cfg(feature = "std")]
-pub use self::extracted_file_provider::{ExtractedFileProviderLocal, ExtractedFileProviderWeb};
 pub use self::package::Package;
 pub use self::reference::SqPackFileHash;
-#[cfg(feature = "std")]
-pub use self::sqpack_reader::SqPackReader;
 pub use self::sqpack_reader_extracted_file::SqPackReaderExtractedFile;
+
+cfg_if::cfg_if! {
+    if #[cfg(feature = "std")] {
+        mod sqpack_reader;
+        pub use self::extracted_file_provider::{ExtractedFileProviderLocal, ExtractedFileProviderWeb};
+        pub use self::sqpack_reader::SqPackReader;
+    }
+}
