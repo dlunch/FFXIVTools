0e3a1b814250a25b3c66612f475fbd60f295dc31
Inseok Lee <dlunch@gmail.com>
2020-04-05 17:46:47 +0900

    Update webpack release setting

--- webpack/webpack.config.release.js
+++ webpack/webpack.config.release.js
@@ -1,13 +1,7 @@
-const webpack = require("webpack");
 const merge = require("webpack-merge");
 const common = require("./webpack.config.common.js");
 
 module.exports = merge.smart(common, {
   mode: "production",
-  devtool: "source-map",
-  devServer: {
-    contentBase: "./dist",
-    hot: true,
-  },
-  plugins: [new webpack.HotModuleReplacementPlugin()],
+  devtool: "",
 });
