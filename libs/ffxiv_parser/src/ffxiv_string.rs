ca755d342c8547b043da7ae1ba52264c971dab88
Inseok Lee <dlunch@gmail.com>
2020-04-11 08:42:38 +0900

    Refactor string

--- libs/ffxiv_parser/src/ffxiv_string.rs
+++ libs/ffxiv_parser/src/ffxiv_string.rs
@@ -6,79 +6,47 @@ pub struct FFXIVString<'a> {
 }
 
 impl<'a> FFXIVString<'a> {
+    const MARKUP_START: u8 = b'\x02';
     pub fn new(data: &'a [u8]) -> Self {
         Self { data }
     }
 
     pub fn decode(&'a self) -> String {
-        self.iter().collect::<String>()
-    }
+        let mut result = String::with_capacity(self.data.len());
+        let mut cursor = 0;
 
-    pub fn iter(&'a self) -> impl Iterator<Item = String> + 'a {
-        FFXIVStringIterator::new(self.data)
-    }
-}
-
-impl<'a> Into<String> for FFXIVString<'a> {
-    fn into(self) -> String {
-        self.decode()
-    }
-}
-
-struct FFXIVStringIterator<'a> {
-    data: &'a [u8],
-    cursor: usize,
-}
-
-impl<'a> FFXIVStringIterator<'a> {
-    const MARKUP_START: u8 = b'\x02';
-
-    pub fn new(data: &'a [u8]) -> Self {
-        Self { data, cursor: 0 }
-    }
-
-    fn next_markup(&mut self) -> String {
-        let markup_type = self.data[self.cursor + 1];
-        self.cursor += 2;
-        let markup_size = self.next_size();
-
-        let result = match markup_type {
-            0x10 => "\n".to_owned(),
-            0x16 => "\u{00AD}".to_owned(), // soft hyphen
-            0x1A => {
-                let payload = self.next_item();
-                match payload {
-                    2 => "<i>",
-                    1 => "</i>",
-                    _ => panic!(),
-                }
-                .to_owned()
+        while cursor < self.data.len() && self.data[cursor] != 0 {
+            if self.data[cursor] == Self::MARKUP_START {
+                result.push_str(&self.next_markup(&mut cursor));
             }
-            0x20 => {
-                let payload = self.next_item();
-                (payload - 1).to_string()
-            }
-            _ => {
-                let payload = &self.data[self.cursor..self.cursor + markup_size];
-                format!(
-                    "<Unknown payload=\"{}\" />",
-                    payload.iter().map(|x| format!("{:x}", x)).collect::<String>()
-                )
-            }
-        };
 
-        self.next_item(); // 0x03
+            let next_offset = self.data[cursor..].iter().position(|&x| x == Self::MARKUP_START || x == 0);
+            if let Some(next_offset) = next_offset {
+                result.push_str(str::from_utf8(&self.data[cursor..next_offset + cursor]).unwrap());
+                cursor += next_offset;
+            } else {
+                result.push_str(str::from_utf8(&self.data[cursor..]).unwrap());
+                break;
+            }
+        }
 
         result
     }
 
-    fn next_size(&mut self) -> usize {
-        let mut next = || self.next_item() as usize;
+    fn next_item(&self, cursor: &mut usize) -> u8 {
+        let item = self.data[*cursor];
+        *cursor += 1;
+
+        item
+    }
+
+    fn next_size(&self, cursor: &mut usize) -> usize {
+        let mut next = || self.next_item(cursor) as usize;
         let item = next();
 
         match item {
             0..=0xEF => item - 1,
-            0xF0 => next() as usize,
+            0xF0 => next(),
             0xF1 => ((next() << 8) | next()) - 1,
             0xF2 => (next() << 8) | next(),
             0xFA => (next() << 16) | (next() << 8) | next(),
@@ -87,34 +55,44 @@ impl<'a> FFXIVStringIterator<'a> {
         }
     }
 
-    fn next_item(&mut self) -> u8 {
-        let item = self.data[self.cursor];
-        self.cursor += 1;
+    fn next_markup(&self, cursor: &mut usize) -> String {
+        let markup_type = self.data[*cursor + 1];
+        *cursor += 2;
+        let markup_size = self.next_size(cursor);
 
-        item
+        let result = match markup_type {
+            0x10 => "\n".to_owned(),
+            0x16 => "\u{00AD}".to_owned(), // soft hyphen
+            0x1A => {
+                let payload = self.next_item(cursor);
+                match payload {
+                    2 => "<i>",
+                    1 => "</i>",
+                    _ => panic!(),
+                }
+                .to_owned()
+            }
+            0x20 => {
+                let payload = self.next_item(cursor);
+                (payload - 1).to_string()
+            }
+            _ => {
+                let payload = &self.data[*cursor..*cursor + markup_size];
+                format!(
+                    "<Unknown payload=\"{}\" />",
+                    payload.iter().map(|x| format!("{:x}", x)).collect::<String>()
+                )
+            }
+        };
+
+        self.next_item(cursor); // 0x03
+
+        result
     }
 }
 
-impl<'a> Iterator for FFXIVStringIterator<'a> {
-    type Item = String;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.cursor >= self.data.len() || self.data[self.cursor] == 0 {
-            return None;
-        }
-
-        if self.data[self.cursor] == Self::MARKUP_START {
-            return Some(self.next_markup());
-        }
-
-        let cursor = self.cursor;
-        let next_offset = self.data[cursor..].iter().position(|&x| x == Self::MARKUP_START || x == 0);
-        if let Some(next_offset) = next_offset {
-            self.cursor += next_offset;
-            Some(str::from_utf8(&self.data[cursor..next_offset + cursor]).unwrap().to_owned())
-        } else {
-            self.cursor = self.data.len();
-            Some(str::from_utf8(&self.data[cursor..]).unwrap().to_owned())
-        }
+impl<'a> Into<String> for FFXIVString<'a> {
+    fn into(self) -> String {
+        self.decode()
     }
 }
