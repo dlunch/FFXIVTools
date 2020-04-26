c981c11a72014c74636866d83469cf4b3af2dda1
Inseok Lee <dlunch@gmail.com>
2020-04-26 11:23:28 +0900

    Do not pass unrequired offset

--- libs/ffxiv_parser/tests/lgb_test.rs
+++ libs/ffxiv_parser/tests/lgb_test.rs
@@ -1,6 +1,6 @@
 #[cfg(test)]
 mod tests {
-    use ffxiv_parser::Lgb;
+    use ffxiv_parser::{LayerGroupResourceItem, Lgb};
     use sqpack_reader::{ExtractedFileProviderWeb, Result, SqPackReaderExtractedFile};
 
     #[tokio::test]
@@ -14,6 +14,11 @@ mod tests {
 
         let lgb = Lgb::new(&pack, "bg/ffxiv/sea_s1/twn/s1t1/level/planner.lgb").await?;
         assert_eq!(lgb.name(), "Planner");
+        let entries = lgb.entries();
+        match entries.get("QST_ClsAcn250_000").unwrap()[0] {
+            LayerGroupResourceItem::EventNpc(x) => assert_eq!(x.item_type, 8),
+            _ => panic!(),
+        }
 
         Ok(())
     }
