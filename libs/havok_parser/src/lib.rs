e3e60ae13be672af0e4e96d4855c979134e3d52e
Inseok Lee <dlunch@gmail.com>
2020-05-28 20:23:23 +0900

    Add animation_container

--- libs/havok_parser/src/lib.rs
+++ libs/havok_parser/src/lib.rs
@@ -1,8 +1,10 @@
+mod animation_container;
 mod binary_tag_file_reader;
 mod object;
 mod skeleton;
 mod transform;
 
+pub use animation_container::HavokAnimationContainer;
 pub use binary_tag_file_reader::HavokBinaryTagFileReader;
 pub use object::HavokValue;
 pub use skeleton::HavokSkeleton;
