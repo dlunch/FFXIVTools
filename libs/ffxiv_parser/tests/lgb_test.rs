34da18fc0d4101faf5fba319452a0a1195338cfa
Inseok Lee <dlunch@gmail.com>
2020-06-07 19:25:00 +0900

    Replace tokio::test to async-std::test

--- libs/ffxiv_parser/tests/lgb_test.rs
+++ libs/ffxiv_parser/tests/lgb_test.rs
@@ -3,7 +3,7 @@ mod tests {
     use ffxiv_parser::{LayerGroupResourceItem, Lgb};
     use sqpack_reader::{ExtractedFileProviderWeb, Result, SqPackReaderExtractedFile};
 
-    #[tokio::test]
+    #[async_std::test]
     async fn lgb_test() -> Result<()> {
         let _ = pretty_env_logger::formatted_timed_builder()
             .filter(Some("sqpack_reader"), log::LevelFilter::Debug)
