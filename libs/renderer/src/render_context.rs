0c9adb419536b81a46d9cc4a5c4511c309fc0327
Inseok Lee <dlunch@gmail.com>
2020-06-13 14:07:07 +0900

    Add renderer to submodule

--- libs/renderer/src/render_context.rs
+++
@@ -1,9 +0,0 @@
-pub struct RenderContext<'a> {
-    pub(crate) render_pass: wgpu::RenderPass<'a>,
-}
-
-impl<'a> RenderContext<'a> {
-    pub fn new(render_pass: wgpu::RenderPass<'a>) -> Self {
-        Self { render_pass }
-    }
-}
