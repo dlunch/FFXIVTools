6fbfba05fe4f91c7f349d33eb6e8a1213017e133
Inseok Lee <dlunch@gmail.com>
2020-06-13 14:19:51 +0900

    Fix unix tests

--- libs/sqpack_reader/tests/read_as_compressed_test.rs
+++ libs/sqpack_reader/tests/read_as_compressed_test.rs
@@ -6,17 +6,18 @@ mod tests {
     async fn read_as_compressed_test() -> sqpack_reader::Result<()> {
         use std::path::Path;
 
-        use sqpack_reader::{ExtractedFileProviderLocal, Package, SqPackReader, SqPackReaderExtractedFile};
+        use sqpack_reader::{ExtractedFileProviderLocal, SqPackFileReference, SqPackReader, SqPackReaderExtractedFile};
 
         let _ = pretty_env_logger::formatted_timed_builder()
             .filter(Some("sqpack_reader"), log::LevelFilter::Debug)
             .try_init();
         {
             let pack = SqPackReader::new(Path::new("/mnt/d/Games/FINAL FANTASY XIV - KOREA/game/sqpack"))?;
-            let pack_file = SqPackReaderExtractedFile::new(ExtractedFileProviderLocal::with_path(Path::new("/mnt/i/FFXIVData/data/kor_510")))?;
+            let pack_file = SqPackReaderExtractedFile::new(ExtractedFileProviderLocal::with_path(Path::new("/mnt/i/FFXIVData/data/kor_510")));
 
             let data = pack.read_as_compressed("exd/item.exh").await?;
-            let data_file = pack_file.read_as_compressed("exd/item.exh").await?;
+            let reference = SqPackFileReference::new("exd/item.exh");
+            let data_file = pack_file.read_as_compressed_by_hash(&reference.hash).await?;
 
             assert_eq!(data, data_file);
         }
