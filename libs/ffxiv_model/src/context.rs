e00cb5606b0dcbec5a6e76bd26df02e0c0a98bb1
Inseok Lee <dlunch@gmail.com>
2020-06-18 20:41:08 +0900

    Create empty texture at ffxiv_model

--- libs/ffxiv_model/src/context.rs
+++ libs/ffxiv_model/src/context.rs
@@ -1,11 +1,14 @@
+use alloc::sync::Arc;
+
 use crate::shader_holder::ShaderHolder;
 use crate::texture_cache::TextureCache;
 
-use renderer::Renderer;
+use renderer::{Renderer, Texture, TextureFormat};
 
 pub struct Context {
     pub(crate) shader_holder: ShaderHolder,
     pub(crate) texture_cache: TextureCache,
+    pub(crate) empty_texture: Arc<Texture>,
 }
 
 impl Context {
@@ -13,6 +16,11 @@ impl Context {
         Self {
             shader_holder: ShaderHolder::new(renderer),
             texture_cache: TextureCache::new(),
+            empty_texture: Self::create_empty_texture(renderer),
         }
+    }
+
+    fn create_empty_texture(renderer: &Renderer) -> Arc<Texture> {
+        Arc::new(Texture::new(renderer, 1, 1, Some(&[0, 0, 0, 0]), TextureFormat::Rgba8Unorm))
     }
 }
