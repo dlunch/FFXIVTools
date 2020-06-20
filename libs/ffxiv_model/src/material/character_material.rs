6403ede404e9dbbc96aaae6fc21f60f51ff6cfa6
Inseok Lee <dlunch@gmail.com>
2020-06-20 10:05:27 +0900

    Add more material

--- libs/ffxiv_model/src/material/character_material.rs
+++ libs/ffxiv_model/src/material/character_material.rs
@@ -1,4 +1,4 @@
-use alloc::{sync::Arc, vec::Vec};
+use alloc::sync::Arc;
 
 use hashbrown::HashMap;
 
@@ -10,19 +10,17 @@ use crate::Context;
 pub struct CharacterMaterial {}
 
 impl CharacterMaterial {
-    pub fn create(renderer: &Renderer, context: &Context, mtrl: &Mtrl, textures: &Vec<Arc<Texture>>) -> Material {
-        let mut textures = mtrl
-            .parameters()
-            .iter()
-            .map(|parameter| (parameter.parameter_type.as_str(), textures[parameter.texture_index as usize].clone()))
-            .collect::<HashMap<_, _>>();
-
+    pub fn create(renderer: &Renderer, context: &Context, mtrl: &Mtrl, mut textures: HashMap<&'static str, Arc<Texture>>) -> Material {
         let color_table_data = mtrl.color_table();
         if !color_table_data.is_empty() {
             let color_table_tex = Texture::new(&renderer, 4, 16, Some(color_table_data), TextureFormat::Rgba16Float);
             textures.insert("ColorTable", Arc::new(color_table_tex));
         } else {
             textures.insert("ColorTable", context.empty_texture.clone());
+        }
+
+        if !textures.contains_key("Mask") {
+            textures.insert("Mask", context.empty_texture.clone());
         }
 
         let shaders = context.shader_holder.get_shaders(mtrl.shader_name());
