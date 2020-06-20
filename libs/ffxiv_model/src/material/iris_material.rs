afd5ce384316dc6471a7dc258bf8ab70ef35e58c
Inseok Lee <dlunch@gmail.com>
2020-06-20 10:05:27 +0900

    Add more material

---
+++ libs/ffxiv_model/src/material/iris_material.rs
@@ -0,0 +1,22 @@
+use alloc::sync::Arc;
+
+use hashbrown::HashMap;
+
+use ffxiv_parser::Mtrl;
+use renderer::{Material, Renderer, Texture};
+
+use crate::Context;
+
+pub struct IrisMaterial {}
+
+impl IrisMaterial {
+    pub fn create(renderer: &Renderer, context: &Context, mtrl: &Mtrl, mut textures: HashMap<&'static str, Arc<Texture>>) -> Material {
+        let shaders = context.shader_holder.get_shaders(mtrl.shader_name());
+
+        if !textures.contains_key("Diffuse") {
+            textures.insert("Diffuse", context.empty_texture.clone());
+        }
+
+        Material::new(&renderer, textures, shaders.0, shaders.1)
+    }
+}
