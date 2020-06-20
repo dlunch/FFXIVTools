66a06358a4502ecf5712bbd922f73877110d27a8
Inseok Lee <dlunch@gmail.com>
2020-06-20 16:52:43 +0900

    Add shadertype

--- libs/ffxiv_model/src/material.rs
+++ libs/ffxiv_model/src/material.rs
@@ -17,9 +17,9 @@ pub async fn create_material(renderer: &Renderer, context: &Context, mtrl: &Mtrl
     let mut textures = gather_textures(mtrl, textures);
     match mtrl.shader_name() {
         "character.shpk" | "characterglass.shpk" => character_material::CharacterMaterial::create(renderer, context, mtrl, &mut textures).await,
-        "hair.shpk" => hair_material::HairMaterial::create(renderer, context, mtrl, &mut textures),
-        "iris.shpk" => iris_material::IrisMaterial::create(renderer, context, mtrl, &mut textures),
-        "skin.shpk" => skin_material::SkinMaterial::create(renderer, context, mtrl, &mut textures),
+        "hair.shpk" => hair_material::HairMaterial::create(renderer, context, &mut textures),
+        "iris.shpk" => iris_material::IrisMaterial::create(renderer, context, &mut textures),
+        "skin.shpk" => skin_material::SkinMaterial::create(renderer, context, &mut textures),
         _ => panic!(),
     }
 }
