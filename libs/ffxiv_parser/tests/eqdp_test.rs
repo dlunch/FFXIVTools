20b80a27adaef7b91ee62933604fef333c947660
Inseok Lee <dlunch@gmail.com>
2020-06-25 19:10:31 +0900

    Add eqdp

---
+++ libs/ffxiv_parser/tests/eqdp_test.rs
@@ -0,0 +1,20 @@
+#[cfg(test)]
+mod tests {
+    use ffxiv_parser::Eqdp;
+    use sqpack_reader::{ExtractedFileProviderWeb, Result, SqPackReaderExtractedFile};
+
+    #[async_std::test]
+    async fn eqdp_test() -> Result<()> {
+        let _ = pretty_env_logger::formatted_timed_builder()
+            .filter(Some("sqpack_reader"), log::LevelFilter::Debug)
+            .try_init();
+
+        let provider = ExtractedFileProviderWeb::new("https://ffxiv-data.dlunch.net/compressed/");
+        let pack = SqPackReaderExtractedFile::new(provider);
+
+        let eqdp = Eqdp::new(&pack, "chara/xls/charadb/equipmentdeformerparameter/c0201.eqdp").await?;
+        assert!(eqdp.has_model(6016, 0));
+
+        Ok(())
+    }
+}
