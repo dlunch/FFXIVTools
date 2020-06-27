deb50073d7915ba0b0ce7fbbefa7d81130fef1b8
Inseok Lee <dlunch@gmail.com>
2020-06-27 12:13:20 +0900

    Bind buffer on material

--- libs/ffxiv_model/src/material/skin_material.rs
+++ libs/ffxiv_model/src/material/skin_material.rs
@@ -13,6 +13,6 @@ impl SkinMaterial {
         let vertex_shader = context.shader_holder.vertex_shader.clone();
         let fragment_shader = context.shader_holder.fragment_shader(ShaderType::Skin);
 
-        Material::new(&renderer, textures, vertex_shader, fragment_shader)
+        Material::new(&renderer, textures, &HashMap::new(), vertex_shader, fragment_shader)
     }
 }
