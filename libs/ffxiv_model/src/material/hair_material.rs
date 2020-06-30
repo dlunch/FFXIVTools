ad09d51c92d07ebef0d5229ea221301d3b7da66b
Inseok Lee <dlunch@gmail.com>
2020-06-30 17:16:07 +0900

    Pass dummy bone transform

--- libs/ffxiv_model/src/material/hair_material.rs
+++ libs/ffxiv_model/src/material/hair_material.rs
@@ -2,14 +2,19 @@ use alloc::sync::Arc;
 
 use hashbrown::HashMap;
 
-use renderer::{Material, Renderer, Texture};
+use renderer::{Buffer, Material, Renderer, Texture};
 
 use crate::{shader_holder::ShaderType, Context};
 
 pub struct HairMaterial {}
 
 impl HairMaterial {
-    pub fn create(renderer: &Renderer, context: &Context, textures: &mut HashMap<&'static str, Arc<Texture>>) -> Material {
+    pub fn create(
+        renderer: &Renderer,
+        context: &Context,
+        textures: &mut HashMap<&'static str, Arc<Texture>>,
+        uniforms: &HashMap<&'static str, Buffer>,
+    ) -> Material {
         let vertex_shader = context.shader_holder.vertex_shader.clone();
         let fragment_shader = context.shader_holder.fragment_shader(ShaderType::Hair);
 
@@ -17,6 +22,6 @@ impl HairMaterial {
             textures.insert("Diffuse", context.empty_texture.clone());
         }
 
-        Material::new(&renderer, textures, &HashMap::new(), vertex_shader, fragment_shader)
+        Material::new(&renderer, textures, uniforms, vertex_shader, fragment_shader)
     }
 }
--- libs/ffxiv_model/src/material/skin_material.rs
+++ libs/ffxiv_model/src/material/skin_material.rs
@@ -2,17 +2,22 @@ use alloc::sync::Arc;
 
 use hashbrown::HashMap;
 
-use renderer::{Material, Renderer, Texture};
+use renderer::{Buffer, Material, Renderer, Texture};
 
 use crate::{shader_holder::ShaderType, Context};
 
 pub struct SkinMaterial {}
 
 impl SkinMaterial {
-    pub fn create(renderer: &Renderer, context: &Context, textures: &mut HashMap<&'static str, Arc<Texture>>) -> Material {
+    pub fn create(
+        renderer: &Renderer,
+        context: &Context,
+        textures: &mut HashMap<&'static str, Arc<Texture>>,
+        uniforms: &HashMap<&'static str, Buffer>,
+    ) -> Material {
         let vertex_shader = context.shader_holder.vertex_shader.clone();
         let fragment_shader = context.shader_holder.fragment_shader(ShaderType::Skin);
 
-        Material::new(&renderer, textures, &HashMap::new(), vertex_shader, fragment_shader)
+        Material::new(&renderer, textures, uniforms, vertex_shader, fragment_shader)
     }
 }
