1be4fab6d6e75cceced88c67f89d2f340d38fe2d
Inseok Lee <dlunch@gmail.com>
2020-07-07 17:40:12 +0900

    Do not use reqwest on non_std

--- libs/sqpack_reader/src/lib.rs
+++ libs/sqpack_reader/src/lib.rs
@@ -15,6 +15,7 @@ pub use archive_id::SqPackArchiveId;
 pub use batched_package::BatchedPackage;
 pub use error::{Result, SqPackReaderError};
 pub use extracted_file_provider::ExtractedFileProvider;
+pub use extracted_file_provider::ExtractedFileProviderWeb;
 pub use package::{BatchablePackage, Package};
 pub use reference::{SqPackFileHash, SqPackFileReference};
 pub use sqpack_reader_extracted_file::SqPackReaderExtractedFile;
@@ -22,7 +23,7 @@ pub use sqpack_reader_extracted_file::SqPackReaderExtractedFile;
 cfg_if::cfg_if! {
     if #[cfg(feature = "std")] {
         mod sqpack_reader;
-        pub use extracted_file_provider::{ExtractedFileProviderLocal, ExtractedFileProviderWeb};
+        pub use extracted_file_provider::ExtractedFileProviderLocal;
         pub use sqpack_reader::SqPackReader;
     }
 }
