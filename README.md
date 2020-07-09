284b0855249bf06528201f95d38e1df94f1a98f7
Inseok Lee <dlunch@gmail.com>
2020-07-09 17:29:05 +0900

    Add web

---
+++ README.md
@@ -0,0 +1,30 @@
+# FFXIVTools
+
+Reimplementation of <https://ffxiv.dlunch.net> in rust.
+
+- Latest `master` branch preview: <https://ffxiv-dev-new.dlunch.net> (Placeholder)
+- Latest stable: <https://ffxiv-new.dlunch.net> (Placeholder)
+
+# Status
+
+Nothing is working on web.
+
+Basic rendering works on PC
+
+![](https://github.com/dlunch/FFXIVTools/raw/master/screenshots/200521.png)
+
+# Build instructions
+
+## Main website
+
+Build
+
+```
+npm run build
+```
+
+Devserver
+
+```
+npm run devserver
+```
---
+++ web/model_viewer/src/app.rs
@@ -0,0 +1,30 @@
+use alloc::vec;
+
+use yew::prelude::{html, Component, ComponentLink, Html, ShouldRender};
+
+pub struct App {}
+
+pub enum Msg {}
+
+impl Component for App {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
+        App {}
+    }
+
+    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
+        true
+    }
+
+    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
+        true
+    }
+
+    fn view(&self) -> Html {
+        html! {
+            <p>{ "Hello world!" }</p>
+        }
+    }
+}
