3f9322f7142b96b596d1ae3ebfc8518933f2c1bd
Inseok Lee <dlunch@gmail.com>
2020-07-09 17:29:05 +0900

    Add web

---
+++ web/translation_compare/src/lib.rs
@@ -0,0 +1,19 @@
+#![no_std]
+extern crate alloc;
+
+mod app;
+
+use wasm_bindgen::prelude::wasm_bindgen;
+
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen(start)]
+pub fn main() {
+    #[cfg(debug_assertions)]
+    console_error_panic_hook::set_once();
+    #[cfg(debug_assertions)]
+    console_log::init_with_level(log::Level::Trace).unwrap();
+
+    yew::start_app::<app::App>();
+}
