e213b99012f50fa45895e8ec941a864a4c409cb6
Inseok Lee <dlunch@gmail.com>
2020-07-13 17:10:49 +0900

    Show ex names

--- web/translation_compare/src/app.rs
+++ web/translation_compare/src/app.rs
@@ -1,33 +1,43 @@
-use alloc::{str, vec};
-
 use wasm_bindgen_futures::spawn_local;
-use web_sys::console;
 use yew::prelude::{html, Component, ComponentLink, Html, ShouldRender};
 
-use sqpack_reader::{ExtractedFileProviderWeb, Package, SqPackReaderExtractedFile};
+use ffxiv_parser::ExList;
+use sqpack_reader::{ExtractedFileProviderWeb, SqPackReaderExtractedFile};
 
-pub struct App {}
+pub struct App {
+    exl: Option<ExList>,
+}
 
-pub enum Msg {}
+pub enum Msg {
+    ExlReady(ExList),
+}
 
 impl Component for App {
     type Message = Msg;
     type Properties = ();
 
-    fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
-        spawn_local(async {
+    fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
+        let callback = link.callback(Msg::ExlReady);
+
+        spawn_local(async move {
             let provider = ExtractedFileProviderWeb::new("https://ffxiv-data.dlunch.net/compressed/");
             let package = SqPackReaderExtractedFile::new(provider);
 
-            let exl = package.read_file("exd/root.exl").await.unwrap();
-            let exl_str = str::from_utf8(&exl).unwrap();
-            console::log_1(&exl_str.into());
+            let exl = ExList::new(&package).await.unwrap();
+
+            callback.emit(exl);
         });
-        App {}
+
+        App { exl: None }
     }
 
-    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
-        true
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            Msg::ExlReady(x) => {
+                self.exl = Some(x);
+                true
+            }
+        }
     }
 
     fn change(&mut self, _props: Self::Properties) -> ShouldRender {
@@ -35,8 +45,15 @@ impl Component for App {
     }
 
     fn view(&self) -> Html {
+        let exds = if let Some(x) = &self.exl {
+            x.ex_names.iter().map(|x| html! { <li>{ x }</li> }).collect::<Html>()
+        } else {
+            html! {}
+        };
         html! {
-            <p>{ "Hello world!" }</p>
+            <ul>
+                { exds }
+            </ul>
         }
     }
 }
