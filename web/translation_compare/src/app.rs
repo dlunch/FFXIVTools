d053b3083979d72e98cd6adec490612018a4025a
Inseok Lee <dlunch@gmail.com>
2020-07-18 18:14:09 +0900

    Read names

--- web/translation_compare/src/app.rs
+++ web/translation_compare/src/app.rs
@@ -3,20 +3,18 @@ use yew::prelude::{html, Component, ComponentLink, Html, ShouldRender};
 
 use ffxiv_exd::{ClassJob, NamedExRow, WrappedEx};
 use ffxiv_parser::Language;
-use sqpack_reader::{ExtractedFileProviderWeb, SqPackReaderExtractedFile};
+use sqpack_reader::{ExtractedFileProviderWeb, Package, Result, SqPackReaderExtractedFile};
 
 use crate::list::List;
 
-pub struct Exs {
-    class_job: WrappedEx<'static, ClassJob<'static>>,
-}
-
 pub struct App {
-    exs: Option<Exs>,
+    link: ComponentLink<Self>,
+    data: Option<Vec<(u32, String)>>,
 }
 
 pub enum Msg {
-    ExsReady(Exs),
+    OnDisplay(&'static str),
+    OnDataReady(Vec<(u32, String)>),
 }
 
 impl Component for App {
@@ -24,24 +22,17 @@ impl Component for App {
     type Properties = ();
 
     fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
-        let callback = link.callback(Msg::ExsReady);
-
-        spawn_local(async move {
-            let provider = ExtractedFileProviderWeb::new("https://ffxiv-data.dlunch.net/compressed/");
-            let package = SqPackReaderExtractedFile::new(provider);
-
-            let class_job = WrappedEx::<ClassJob>::new(&package).await.unwrap();
-
-            callback.emit(Exs { class_job });
-        });
-
-        Self { exs: None }
+        Self { link, data: None }
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
-            Msg::ExsReady(x) => {
-                self.exs = Some(x);
+            Msg::OnDisplay(x) => {
+                self.display_result(x);
+                true
+            }
+            Msg::OnDataReady(x) => {
+                self.data = Some(x);
                 true
             }
         }
@@ -52,35 +43,51 @@ impl Component for App {
     }
 
     fn view(&self) -> Html {
-        if let Some(x) = &self.exs {
-            let items = x
-                .class_job
-                .all(Language::English)
-                .unwrap()
-                .map(|(_, value)| html! { <li>{ value.name() }</li> })
-                .collect::<Html>();
-            html! {
-                <ul>
-                    { items }
-                </ul>
-            }
-        } else {
-            html! {
-                <div>
-                    <span>
-                        <button>{ "classjob" }</button>
-                        <button>{ "item" }</button>
-                        <button>{ "action" }</button>
-                        <button>{ "craftaction" }</button>
-                        <button>{ "enemy" }</button>
-                        <button>{ "npc" }</button>
-                        <button>{ "quest" }</button>
-                        <button>{ "place" }</button>
-                    </span>
-                    <List>
-                    </List>
-                </div>
-            }
+        let buttons = ["classjob", "item", "action", "craftaction", "enemy", "npc", "quest", "place"]
+            .iter()
+            .map(|x| {
+                html! {
+                    <button onclick=self.link.callback(move |_| Msg::OnDisplay(x))>{ x }</button>
+                }
+            })
+            .collect::<Html>();
+
+        html! {
+            <div>
+                <span>
+                    { buttons }
+                </span>
+                <List>
+                </List>
+            </div>
         }
+    }
+}
+
+impl App {
+    fn display_result(&self, name: &'static str) {
+        let callback = self.link.callback(Msg::OnDataReady);
+
+        spawn_local(async move {
+            let provider = ExtractedFileProviderWeb::new("https://ffxiv-data.dlunch.net/compressed/");
+            let package = SqPackReaderExtractedFile::new(provider);
+
+            let names = match name {
+                "classjob" => Self::read_names::<ClassJob>(&package, Language::English).await.unwrap(),
+                _ => panic!(),
+            };
+
+            callback.emit(names);
+        });
+    }
+
+    async fn read_names<'a, T: NamedExRow<'static> + 'static>(package: &dyn Package, language: Language) -> Result<Vec<(u32, String)>> {
+        let wrapped_ex = WrappedEx::<T>::new(package).await?;
+
+        // TODO do we really require unsafe here??
+        let wrapped_ex_ref: &WrappedEx<T> = unsafe { core::mem::transmute(&wrapped_ex) };
+        let all = wrapped_ex_ref.all(language).unwrap();
+
+        Ok(all.map(|(k, v)| (k, v.name())).collect::<Vec<_>>())
     }
 }
