12332cf79568402d12cad8321b2b91482d6d1285
Inseok Lee <dlunch@gmail.com>
2020-07-30 17:19:48 +0900

    Add bone name

--- libs/ffxiv_parser/tests/mdl_test.rs
+++ libs/ffxiv_parser/tests/mdl_test.rs
@@ -12,33 +12,38 @@ mod tests {
         let provider = ExtractedFileProviderWeb::new("https://ffxiv-data.dlunch.net/compressed/all/");
         let pack = SqPackReaderExtractedFile::new(provider);
 
-        let mdl = Mdl::new(&pack, "bg/ex1/01_roc_r2/common/bgparts/r200_a0_bari1.mdl").await?;
+        let mdl = Mdl::new(&pack, "chara/equipment/e0100/model/c1101e0100_top.mdl").await?;
         let buffer_item = mdl.buffer_items(0).next().unwrap().items().next().unwrap();
-        assert!(buffer_item.item_type == BufferItemType::Half4);
+        assert!(buffer_item.item_type == BufferItemType::Float3);
         assert!(buffer_item.usage == BufferItemUsage::Position);
 
         {
             let meshes = mdl.meshes(0).collect::<Vec<_>>();
-            assert_eq!(meshes.len(), 1);
-            assert_eq!(meshes[0].mesh_info.vertex_count, 2790);
+            assert_eq!(meshes.len(), 2);
+            assert_eq!(meshes[0].mesh_info.vertex_count, 5727);
             assert_eq!(meshes[0].buffers.len(), 2);
         }
         {
             let meshes = mdl.meshes(1).collect::<Vec<_>>();
-            assert_eq!(meshes.len(), 1);
-            assert_eq!(meshes[0].mesh_info.vertex_count, 1621);
+            assert_eq!(meshes.len(), 2);
+            assert_eq!(meshes[0].mesh_info.vertex_count, 3307);
             assert_eq!(meshes[0].buffers.len(), 2);
         }
 
         {
             let meshes = mdl.meshes(2).collect::<Vec<_>>();
-            assert_eq!(meshes.len(), 1);
-            assert_eq!(meshes[0].mesh_info.vertex_count, 298);
+            assert_eq!(meshes.len(), 2);
+            assert_eq!(meshes[0].mesh_info.vertex_count, 1731);
             assert_eq!(meshes[0].buffers.len(), 2);
         }
 
+        {
+            let bone_names = mdl.bone_names(0).collect::<Vec<_>>();
+            assert_eq!(bone_names[0], "j_asi_a_l");
+        }
+
         let materials = mdl.material_paths().collect::<Vec<_>>();
-        assert_eq!(materials[0], "bg/ex1/01_roc_r2/common/material/r200_b0_bari1a.mtrl");
+        assert_eq!(materials[0], "/mt_c0101e0100_top_a.mtrl");
 
         Ok(())
     }
