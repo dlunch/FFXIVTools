adaac4f452a4d08f9ae447b6c5da0ad8dd9b1b8d
Inseok Lee <dlunch@gmail.com>
2020-08-01 16:29:58 +0900

    Sort use

--- libs/ffxiv_model/src/character_equipment_part.rs
+++ libs/ffxiv_model/src/character_equipment_part.rs
@@ -15,10 +15,11 @@ impl CharacterEquipmentPart {
     pub async fn new(
         renderer: &Renderer,
         model_data: EquipmentModelData,
-        bone_transforms: &HashMap<String, Matrix4<f32>>,
+        _bone_transforms: &HashMap<String, Matrix4<f32>>,
         context: &Context,
     ) -> Self {
-        let part = CharacterPart::new(renderer, model_data.model_data, bone_transforms, context).await;
+        let prebone_deformer = context.get_body_deform_matrices(model_data.original_body_id, model_data.deformed_body_id);
+        let part = CharacterPart::new(renderer, model_data.model_data, &prebone_deformer, context).await;
 
         Self { part }
     }
