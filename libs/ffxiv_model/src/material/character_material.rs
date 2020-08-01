cdd43c3db60557bf0bc7737c899eb21ac3a262de
Inseok Lee <dlunch@gmail.com>
2020-08-01 14:58:27 +0900

    Use bone transform hashmap

--- libs/ffxiv_model/src/material/character_material.rs
+++ libs/ffxiv_model/src/material/character_material.rs
@@ -1,4 +1,5 @@
 use alloc::sync::Arc;
+use core::future::Future;
 
 use hashbrown::HashMap;
 
@@ -10,28 +11,31 @@ use crate::{shader_holder::ShaderType, Context};
 pub struct CharacterMaterial {}
 
 impl CharacterMaterial {
-    pub async fn create(
-        renderer: &Renderer,
-        context: &Context,
-        mtrl: &Mtrl,
-        textures: &mut HashMap<&'static str, Arc<Texture>>,
-        uniforms: &HashMap<&'static str, Arc<Buffer>>,
-    ) -> Material {
-        let color_table_data = mtrl.color_table();
-        if !color_table_data.is_empty() {
-            let color_table_tex = Texture::with_texels(&renderer, 4, 16, color_table_data, TextureFormat::Rgba16Float).await;
-            textures.insert("ColorTable", Arc::new(color_table_tex));
-        } else {
-            textures.insert("ColorTable", context.empty_texture.clone());
+    pub fn create<'a>(
+        renderer: &'a Renderer,
+        context: &'a Context,
+        mtrl: &'a Mtrl,
+        mut textures: HashMap<&'static str, Arc<Texture>>,
+        uniforms: HashMap<&'static str, Arc<Buffer>>,
+    ) -> impl Future<Output = Material> + 'a {
+        // NOTE return async closure to workaround https://github.com/rust-lang/rust/issues/63033
+        async move {
+            let color_table_data = mtrl.color_table();
+            if !color_table_data.is_empty() {
+                let color_table_tex = Texture::with_texels(&renderer, 4, 16, color_table_data, TextureFormat::Rgba16Float).await;
+                textures.insert("ColorTable", Arc::new(color_table_tex));
+            } else {
+                textures.insert("ColorTable", context.empty_texture.clone());
+            }
+
+            if !textures.contains_key("Mask") {
+                textures.insert("Mask", context.empty_texture.clone());
+            }
+
+            let vertex_shader = context.shader_holder.vertex_shader.clone();
+            let fragment_shader = context.shader_holder.fragment_shader(ShaderType::Character);
+
+            Material::new(&renderer, textures, uniforms, vertex_shader, fragment_shader)
         }
-
-        if !textures.contains_key("Mask") {
-            textures.insert("Mask", context.empty_texture.clone());
-        }
-
-        let vertex_shader = context.shader_holder.vertex_shader.clone();
-        let fragment_shader = context.shader_holder.fragment_shader(ShaderType::Character);
-
-        Material::new(&renderer, &textures, uniforms, vertex_shader, fragment_shader)
     }
 }
