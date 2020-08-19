d2d25fcbf92201595135a0de717ec29a52df7a52
Inseok Lee <dlunch@gmail.com>
2020-08-19 19:22:22 +0900

    Fix test

--- libs/sqpack_reader/tests/read_files_test.rs
+++ libs/sqpack_reader/tests/read_files_test.rs
@@ -9,7 +9,7 @@ mod tests {
             .filter(Some("sqpack_reader"), log::LevelFilter::Debug)
             .try_init();
 
-        let provider = ExtractedFileProviderWeb::new("https://ffxiv-data.dlunch.net/compressed/all/");
+        let provider = ExtractedFileProviderWeb::new("https://ffxiv-data.dlunch.net/compressed/global_520/");
         let pack = SqPackReaderExtractedFile::new(provider);
         let (reference1, reference2, reference3) = (
             SqPackFileReference::new("exd/item.exh"),
