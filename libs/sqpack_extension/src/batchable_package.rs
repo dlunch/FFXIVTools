3f4a05263efc586e2735a23cbcd06c4cc2dedb32
Inseok Lee <dlunch@gmail.com>
2020-08-30 17:17:45 +0900

    Fix build on non_std

--- libs/sqpack_extension/src/batchable_package.rs
+++ libs/sqpack_extension/src/batchable_package.rs
@@ -1,27 +1,33 @@
 use alloc::{boxed::Box, vec::Vec};
 
 use async_trait::async_trait;
-use futures::{
-    future::FutureExt,
-    stream::{FuturesUnordered, TryStreamExt},
-};
 use hashbrown::HashMap;
 
-use sqpack::{Package, Result, SqPackFileReference, SqPackReader};
+use sqpack::{Result, SqPackFileReference};
 
 #[async_trait]
 pub trait BatchablePackage: Sync + Send {
     async fn read_files(&self, references: &[&SqPackFileReference]) -> Result<HashMap<SqPackFileReference, Vec<u8>>>;
 }
 
-#[async_trait]
-impl BatchablePackage for SqPackReader {
-    async fn read_files(&self, references: &[&SqPackFileReference]) -> Result<HashMap<SqPackFileReference, Vec<u8>>> {
-        references
-            .iter()
-            .map(|reference| self.read_file_by_reference(reference).map(move |x| Ok(((*reference).to_owned(), x?))))
-            .collect::<FuturesUnordered<_>>()
-            .try_collect::<HashMap<_, _>>()
-            .await
+cfg_if::cfg_if! {
+    if #[cfg(feature = "std")] {
+        use futures::{
+            future::FutureExt,
+            stream::{FuturesUnordered, TryStreamExt},
+        };
+        use sqpack::{Package, SqPackReader};
+
+        #[async_trait]
+        impl BatchablePackage for SqPackReader {
+            async fn read_files(&self, references: &[&SqPackFileReference]) -> Result<HashMap<SqPackFileReference, Vec<u8>>> {
+                references
+                    .iter()
+                    .map(|reference| self.read_file_by_reference(reference).map(move |x| Ok(((*reference).to_owned(), x?))))
+                    .collect::<FuturesUnordered<_>>()
+                    .try_collect::<HashMap<_, _>>()
+                    .await
+            }
+        }
     }
 }
