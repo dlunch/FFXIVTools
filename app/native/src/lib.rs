fcc68462efb6ec19351a82c56ea9a27d19b9264d
Inseok Lee <dlunch@gmail.com>
2020-10-27 09:27:42 +0900

    Initial native module

--- app/native/src/lib.rs
+++ app/native/src/lib.rs
@@ -3,48 +3,19 @@
 #![allow(clippy::unnecessary_mut_passed)]
 #![cfg(not(test))]
 
-use std::convert::TryInto;
-
-use napi::{register_module, CallContext, Env, JsNumber, JsObject, Module, Result, Task};
+use napi::{register_module, CallContext, JsObject, Module, Result};
 use napi_derive::js_function;
 
-register_module!(example, init);
-
-struct AsyncTask(u32);
-
-impl Task for AsyncTask {
-    type Output = u32;
-    type JsValue = JsNumber;
-
-    fn compute(&mut self) -> Result<Self::Output> {
-        use std::thread::sleep;
-        use std::time::Duration;
-        sleep(Duration::from_millis(self.0 as u64));
-        Ok(self.0 * 2)
-    }
-
-    fn resolve(&self, env: &mut Env, output: Self::Output) -> Result<Self::JsValue> {
-        env.create_uint32(output)
-    }
-}
+register_module!(native, init);
 
 fn init(module: &mut Module) -> Result<()> {
-    module.create_named_method("sync", sync_fn)?;
-
-    module.create_named_method("sleep", sleep)?;
+    module.create_named_method("create", create)?;
     Ok(())
 }
 
-#[js_function(1)]
-fn sync_fn(ctx: CallContext) -> Result<JsNumber> {
-    let argument: u32 = ctx.get::<JsNumber>(0)?.try_into()?;
+#[js_function(0)]
+fn create(ctx: CallContext) -> Result<JsObject> {
+    let result = ctx.env.create_object()?;
 
-    ctx.env.create_uint32(argument + 100)
-}
-
-#[js_function(1)]
-fn sleep(ctx: CallContext) -> Result<JsObject> {
-    let argument: u32 = ctx.get::<JsNumber>(0)?.try_into()?;
-    let task = AsyncTask(argument);
-    ctx.env.spawn(task)
+    Ok(result)
 }
