244d55dea4d0c3af09e962f717f99a210d258ea5
Inseok Lee <dlunch@gmail.com>
2020-10-28 18:57:20 +0900

    Remove unrequired lifetime

--- client/common/src/wasm_package.rs
+++ client/common/src/wasm_package.rs
@@ -1,7 +1,7 @@
 use crate::Region;
 
 use alloc::{boxed::Box, format, sync::Arc, vec::Vec};
-use core::{marker::PhantomData, time::Duration};
+use core::time::Duration;
 
 use async_trait::async_trait;
 use wasm_bindgen_futures::spawn_local;
@@ -10,12 +10,11 @@ use wasm_timer::Delay;
 use sqpack::{Package, Result, SqPackFileReference};
 use sqpack_extension::{BatchedPackage, ExtractedFileProviderWeb, SqPackReaderExtractedFile};
 
-pub struct WasmPackage<'a> {
-    package: Arc<BatchedPackage<'a>>,
-    phantom: PhantomData<&'a u8>,
+pub struct WasmPackage {
+    package: Arc<BatchedPackage>,
 }
 
-impl<'a> WasmPackage<'a> {
+impl WasmPackage {
     pub fn new(region: &Region) -> Self {
         let uri = format!("{}_{}", region.name, region.version);
         let provider = ExtractedFileProviderWeb::new(&format!("https://ffxiv-data.dlunch.net/compressed/{}/", uri));
@@ -34,15 +33,12 @@ impl<'a> WasmPackage<'a> {
             }
         });
 
-        Self {
-            package: result,
-            phantom: PhantomData,
-        }
+        Self { package: result }
     }
 }
 
 #[async_trait]
-impl Package for WasmPackage<'_> {
+impl Package for WasmPackage {
     async fn read_file_by_reference(&self, reference: &SqPackFileReference) -> Result<Vec<u8>> {
         self.package.read_file_by_reference(reference).await
     }
