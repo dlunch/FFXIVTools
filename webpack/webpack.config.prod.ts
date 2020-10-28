f0c21cabc78ee0d235cc23b41d5c6868048db5be
Inseok Lee <dlunch@gmail.com>
2020-10-28 16:12:22 +0900

    Extract css on prod

--- webpack/webpack.config.prod.ts
+++ webpack/webpack.config.prod.ts
@@ -1,7 +1,32 @@
+import * as MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import * as webpack from 'webpack';
+
 import merge from 'webpack-merge';
 import common from './webpack.config.common';
 
 export default merge(common, {
   mode: 'production',
   devtool: undefined,
+  module: {
+    rules: [
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              esModule: false,
+            },
+          },
+          {
+            loader: 'less-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [new MiniCssExtractPlugin() as webpack.WebpackPluginInstance],
 });
