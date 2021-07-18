014cd8e39e422b971b2097a3c8a68d341ab00b3c
Inseok Lee <git@dlun.ch>
2021-07-18 19:13:34 +0900

    Winit event loop on model_viewer

--- client/model_viewer/src/lib.rs
+++ client/model_viewer/src/lib.rs
@@ -2,7 +2,14 @@ extern crate alloc;
 
 mod app;
 
+use log::debug;
 use wasm_bindgen::prelude::wasm_bindgen;
+use winit::{
+    event,
+    event::WindowEvent,
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
 
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
@@ -11,6 +18,50 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 pub fn main() {
     #[cfg(debug_assertions)]
     console_error_panic_hook::set_once();
+    #[cfg(debug_assertions)]
+    console_log::init_with_level(log::Level::Trace).unwrap();
 
-    yew::start_app::<app::App>();
+    let event_loop = EventLoop::new();
+    let builder = WindowBuilder::new();
+
+    yew::initialize();
+
+    #[cfg(target_arch = "wasm32")]
+    {
+        use web_sys::HtmlCanvasElement;
+        use winit::platform::web::WindowBuilderExtWebSys;
+
+        let link = yew::App::<app::App>::new().mount_to_body();
+        let component = link.get_component().unwrap();
+        builder = builder.with_canvas(component.canvas.cast::<HtmlCanvasElement>());
+    }
+
+    let window = builder.build(&event_loop).unwrap();
+
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Poll;
+
+        match event {
+            event::Event::MainEventsCleared => window.request_redraw(),
+            event::Event::WindowEvent { event, .. } => match event {
+                WindowEvent::KeyboardInput {
+                    input:
+                        event::KeyboardInput {
+                            virtual_keycode: Some(event::VirtualKeyCode::Escape),
+                            state: event::ElementState::Pressed,
+                            ..
+                        },
+                    ..
+                }
+                | WindowEvent::CloseRequested => {
+                    *control_flow = ControlFlow::Exit;
+                }
+                _ => {}
+            },
+            event::Event::RedrawRequested(_) => {
+                debug!("Redraw");
+            }
+            _ => {}
+        }
+    });
 }
