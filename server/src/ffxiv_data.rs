525b195a7d356055999f882dcab94526c4622795
Inseok Lee <git@dlun.ch>
2022-05-18 19:51:33 +0900

    Allow unversioned routes

--- server/src/ffxiv_data.rs
+++ server/src/ffxiv_data.rs
@@ -68,6 +68,13 @@ async fn get_exl(context: Extension<Context>, Path(version): Path<String>) -> Re
     Ok(Json(exl.ex_names))
 }
 
+async fn get_ex_all(context: Extension<Context>, Path((version, ex_name)): Path<(String, String)>) -> Result<Json<serde_json::Value>, StatusCode> {
+    let package = context.packages.get(&version).ok_or(StatusCode::NOT_FOUND)?;
+    let result = ex_to_json(package, None, &ex_name).await.map_err(|_| StatusCode::NOT_FOUND)?;
+
+    Ok(Json(result))
+}
+
 async fn get_ex(
     context: Extension<Context>,
     Path((version, language, ex_name)): Path<(String, u16, String)>,
@@ -78,6 +85,23 @@ async fn get_ex(
         .map_err(|_| StatusCode::NOT_FOUND)?;
 
     Ok(Json(result))
+}
+
+async fn get_ex_bulk_all(
+    context: Extension<Context>,
+    Path((version, ex_names)): Path<(String, String)>,
+) -> Result<Json<BTreeMap<String, serde_json::Value>>, StatusCode> {
+    let package = context.packages.get(&version).ok_or(StatusCode::NOT_FOUND)?;
+    let ex_jsons = ex_names
+        .split('.')
+        .map(|ex_name| {
+            ex_to_json(package, None, ex_name).map(move |data| Ok::<_, StatusCode>((ex_name.to_owned(), data.map_err(|_| StatusCode::NOT_FOUND)?)))
+        })
+        .collect::<FuturesUnordered<_>>()
+        .try_collect::<BTreeMap<_, _>>()
+        .await?;
+
+    Ok(Json(ex_jsons))
 }
 
 async fn get_ex_bulk(
@@ -120,6 +144,13 @@ async fn get_lvb(context: Extension<Context>, Path((version, path)): Path<(Strin
     Ok(Json(JsonLvb { layers }))
 }
 
+async fn get_compressed_all(
+    context: Extension<Context>,
+    Path((folder_hash, file_hash, path_hash)): Path<(u32, u32, u32)>,
+) -> Result<Vec<u8>, StatusCode> {
+    get_compressed(context, Path(("all".into(), folder_hash, file_hash, path_hash))).await
+}
+
 async fn get_compressed(
     context: Extension<Context>,
     Path((version, folder_hash, file_hash, path_hash)): Path<(String, u32, u32, u32)>,
@@ -132,6 +163,10 @@ async fn get_compressed(
         .map_err(|_| StatusCode::NOT_FOUND)?;
 
     Ok(result)
+}
+
+async fn get_compressed_bulk_all(context: Extension<Context>, Path(paths): Path<String>) -> Result<Vec<u8>, StatusCode> {
+    get_compressed_bulk(context, Path(("all".into(), paths))).await
 }
 
 async fn get_compressed_bulk(context: Extension<Context>, Path((version, paths)): Path<(String, String)>) -> Result<Vec<u8>, StatusCode> {
@@ -191,11 +226,16 @@ pub fn router() -> Router {
     let context = Context::new().unwrap();
 
     Router::new()
-        .route("/compressed/:version/:folder_hash/:file_hash/:path_hash", get(get_compressed))
-        .route("/compressed/:version/bulk/*paths", get(get_compressed_bulk))
+        // intentionally named as a, b, c, d to workaround https://github.com/ibraheemdev/matchit/issues/13
+        .route("/compressed/:a/bulk/*paths", get(get_compressed_bulk))
+        .route("/compressed/bulk/*paths", get(get_compressed_bulk_all))
+        .route("/compressed/:a/:b/:c/:d", get(get_compressed))
+        .route("/compressed/:a/:b/:c", get(get_compressed_all))
         .route("/parsed/exl/:version", get(get_exl))
-        .route("/parsed/ex/:version/:language/:ex_name", get(get_ex))
-        .route("/parsed/ex/bulk/:version/:language/:ex_names", get(get_ex_bulk))
+        .route("/parsed/ex/:a/:b/:c", get(get_ex))
+        .route("/parsed/ex/:a/:b", get(get_ex_all))
+        .route("/parsed/ex/bulk/:a/:b/:c", get(get_ex_bulk))
+        .route("/parsed/ex/bulk/:a/:b", get(get_ex_bulk_all))
         .route("/parsed/lvb/:version/*path", get(get_lvb))
         .layer(Extension(context))
 }
