e5b3d85e161c7f1215fd28916e6e3c11d3b13812
Inseok Lee <git@dlun.ch>
2025-04-27 17:01:21 +0900

    Mtrl test fix

--- libs/ffxiv_parser/tests/mtrl_test.rs
+++ libs/ffxiv_parser/tests/mtrl_test.rs
@@ -19,11 +19,11 @@ async fn mtrl_test() -> Result<()> {
         );
 
         let color_table = mtrl.color_table();
-        assert_eq!(color_table.len(), 544);
+        assert_eq!(color_table.len(), 2176);
         assert_eq!(color_table[0], 0x00u8);
         assert_eq!(color_table[1], 0x3cu8);
 
-        assert_eq!(mtrl.shader_name(), "character.shpk");
+        assert_eq!(mtrl.shader_name(), "characterlegacy.shpk");
 
         assert!(mtrl.parameters()[0].parameter_type == MtrlParameterType::Normal)
     }
@@ -32,7 +32,7 @@ async fn mtrl_test() -> Result<()> {
         let mtrl = Mtrl::new(&pack, "chara/human/c0201/obj/body/b0001/material/v0001/mt_c0201b0001_a.mtrl").await?;
         assert!(
             mtrl.texture_paths()
-                .any(|x| x == "chara/human/c0201/obj/body/b0001/texture/--c0201b0001_d.tex")
+                .any(|x| x == "chara/human/c0201/obj/body/b0001/texture/c0201b0001_base.tex")
         );
     }
 
